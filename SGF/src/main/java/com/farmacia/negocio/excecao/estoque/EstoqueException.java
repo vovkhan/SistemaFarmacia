91a62a43e1cf5db5900f1dfddc35bf0f24cb02ef
Guilherme Henrique <guihbslima200@gmail.com>
2025-08-18 23:49:05 -0300

    Criação das Exceções Base

---
+++ SGF/src/main/java/com/farmacia/negocio/excecao/estoque/EstoqueException.java
@@ -0,0 +1,7 @@
+package com.farmacia.negocio.excecao.estoque;
+
+public class EstoqueException extends RuntimeException {
+    public EstoqueException(String message) {
+        super(message);
+    }
+}
