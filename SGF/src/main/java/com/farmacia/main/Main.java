4687bb46cc19a95df43cee36d32acb23ea5496b4
Guilherme Henrique <guihbslima200@gmail.com>
2025-08-18 23:35:54 -0300

    Fix 4.0

--- SGF/src/main/java/com/farmacia/main/Main.java
+++ SGF/src/main/java/com/farmacia/main/Main.java
@@ -3,5 +3,10 @@ package com.farmacia.main;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hoi hoi hoi");
+        int i = 2;
+        int j = 2;
+
+        System.out.println(i+j);
+
     }
 }
