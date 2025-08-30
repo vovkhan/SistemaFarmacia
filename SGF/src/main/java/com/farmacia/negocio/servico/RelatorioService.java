4a1126e008f6465010ecd949d852195619403fae
Guilherme Henrique <guihbslima200@gmail.com>
2025-08-30 08:04:14 -0300

    Limpeza

--- SGF/src/main/java/com/farmacia/negocio/servico/RelatorioService.java
+++ SGF/src/main/java/com/farmacia/negocio/servico/RelatorioService.java
@@ -7,7 +7,6 @@ import com.farmacia.dados.repositorio.IRepositorioVendas;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class RelatorioService {
 
